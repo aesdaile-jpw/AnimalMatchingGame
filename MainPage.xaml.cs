51b5e1a1aaa62c8528fa38cab7171f87f54cca75
Adrian Esdaile <adrian.esdaile@jpw.com.au>
2025-03-18 11:48:29 +1100

    page 48

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -13,6 +13,25 @@
         {
             AnimalButtons.IsVisible = true;
             PlayAgainButton.IsVisible = false;
+
+            List<string> animalEmoji = [ 
+                "🐬","🐬",
+                "🐘","🐘",
+                "🦘","🦘",
+                "🐄","🐄",
+                "😻","😻",
+                "🦒","🦒",
+                "🐯","🐯",
+                "🐰","🐰"
+                ];
+            foreach (var button in AnimalButtons.Children.OfType<Button>())
+            {
+                int index = Random.Shared.Next(animalEmoji.Count);
+                string nextEmoji = animalEmoji[index];
+                button.Text = nextEmoji;
+                animalEmoji.RemoveAt(index);
+            }
+
         }
 
         private void Button_Clicked(object sender, EventArgs e)
