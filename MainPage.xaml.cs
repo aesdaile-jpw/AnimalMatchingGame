86f621936d3e0ac334f2fa91293863b25438cb73
Adrian Esdaile <adrian.esdaile@jpw.com.au>
2025-03-19 21:36:17 +1100

    pag e63 but it doesnt run right

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -32,6 +32,27 @@
                 animalEmoji.RemoveAt(index);
             }
 
+            Dispatcher.StartTimer(TimeSpan.FromSeconds(.1), TimerTick);
+
+        }
+
+
+        int tenthsOfSecondsElapsed = 0;
+        private bool TimerTick()
+        {
+            if (!this.IsLoaded) return false;
+
+            tenthsOfSecondsElapsed++;
+
+            TimeElapsed.Text = "Time elapsed: " + (tenthsOfSecondsElapsed / 10F).ToString("0.0s");
+
+            if (PlayAgainButton.IsVisible)
+            {
+                tenthsOfSecondsElapsed = 0;
+                return false;
+            }
+
+            return true;
         }
 
         Button lastClicked;
@@ -49,8 +70,7 @@
                 }
                 else
                 {
-                    if ((buttonClicked != lastClicked) && (buttonClicked.Text == lastClicked.Text)
-                        && (!string.IsNullOrWhiteSpace(buttonClicked.Text)))
+                    if ((buttonClicked != lastClicked) && (buttonClicked.Text == lastClicked.Text))
                     {
                         matchesFound++;
                         lastClicked.Text = " ";
@@ -62,11 +82,7 @@
                 }
             }
             if (matchesFound == 8)
-            {
-                matchesFound = 0;
-                AnimalButtons.IsVisible = false;
-                PlayAgainButton.IsVisible = true;
-            }
+            { }
         }
     }
 
